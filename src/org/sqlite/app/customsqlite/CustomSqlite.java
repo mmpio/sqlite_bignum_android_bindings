873b5183809705795fbc5837a6a568d3d9de8ebc
dan <dan>
2013-12-18 17:36:15 +0000

    Update CustomSqlite.java to call "SELECT sqlite_version()".

--- src/org/sqlite/app/customsqlite/CustomSqlite.java
+++ src/org/sqlite/app/customsqlite/CustomSqlite.java
@@ -1,7 +1,19 @@
+
 package org.sqlite.app.customsqlite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.sqlite.database.sqlite.SQLiteDatabase;
+import org.sqlite.database.sqlite.SQLiteStatement;
+
+/*
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+*/
 
 public class CustomSqlite extends Activity
 {
@@ -11,5 +23,32 @@ public class CustomSqlite extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void find_version(View view){
+
+      System.loadLibrary("sqliteX");
+
+      SQLiteDatabase db = null;
+      SQLiteStatement st;
+      String res;
+
+      TextView tv = (TextView)findViewById(R.id.tv_widget);
+      tv.setText("<attempting to access sqlite...>");
+
+      try {
+        db = SQLiteDatabase.openOrCreateDatabase(":memory:", null);
+        st = db.compileStatement("SELECT sqlite_version()");
+        res = st.simpleQueryForString();
+
+        tv.setText(res);
+            
+      }catch(Exception e){
+        Log.e("Error", "Error", e);
+        tv.setText(e.toString());
+      } finally {
+        if (db != null)
+          db.close();
+      }
     }
 }
