f845a20b0b63e9458479f76b8732e95bf9fa797f
dan <dan>
2013-12-18 16:50:29 +0000

    Empty android package.

---
+++ src/org/sqlite/app/customsqlite/CustomSqlite.java
@@ -0,0 +1,15 @@
+package org.sqlite.app.customsqlite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomSqlite extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
